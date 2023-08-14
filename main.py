cc9fc75e6c38090bd3d06d55149d7901d5f4468d
sahis2002 <89941586+SahisN@users.noreply.github.com>
2023-08-13 20:21:15 -0700

    changed main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request
 import cv2
 import os
 import signal
+
+import RPi.GPIO as GPIO
+from PCA9685 import PCA9685
 
 app = Flask(__name__)
 
@@ -33,18 +36,32 @@ def video_feed():
 
 @app.route("/button_press/<direction>")
 def button_press(direction):
+    x = int(request.args.get("x"))  # Get the x value from the URL parameters
+    y = int(request.args.get("y"))  # Get the y value from the URL parameters
+
+    print(x)
+    print(y)
+
     # print(f"Button pressed: {direction}")
     if direction == "up":
         print("moving up")
+        pwm.setRotationAngle(1, y)
 
     elif direction == "down":
         print("moving down")
+        pmw.setRotationAngle(1, y)
 
     elif direction == "right":
         print("moving right")
+        pmw.setRotationAngle(0, x)
 
     elif direction == "left":
         print("moving left")
+        pmw.setRotationAngle(0, x)
+
+    # Process the direction, x_value, and y_value as needed
+    # For example, you might print or log them
+    # print(f"Direction: {direction}, X Value: {x_value}, Y Value: {y_value}")
 
     return "OK"
 
@@ -52,10 +69,15 @@ def button_press(direction):
 @app.route("/shutdown", methods=["POST"])
 def shutdown():
     print("Shutting down the server...")
+    pwm.exit_PCA9685()
     os.kill(os.getpid(), signal.SIGINT)
 
     return "Server shutting down..."
 
 
 if __name__ == "__main__":
+    pmw = PCA9685()
+    pmw.setPWMFreq(50)
+    pwm.setRotationAngle(0, x)
+    pwm.setRotationAngle(1, y)
     app.run(host="0.0.0.0", debug=True)
